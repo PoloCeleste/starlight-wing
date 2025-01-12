ffd45cda5d890a257418a25ecd9bc4073c72d5e9
PoloCeleste <voidness12@gmail.com>
2025-01-12 19:55:20 +0900

    [Fix] Fix Configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,19 +6,32 @@ module.exports = {
     description: `천체 달력과 기상 정보를 확인하고, 별자리를 생성하며, 관측 일기와 천체 사진을 공유할 수 있는 종합 천문 플랫폼`,
     author: `@Chanaping`,
   },
-  graphqlTypegen: true,
-  plugins: [`gatsby-plugin-typescript`, `gatsby-plugin-postcss`],
+  flags: {
+    DEV_SSR: true,
+  },
+  plugins: [
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true,
+        jsxPragma: `React`,
+        allExtensions: true,
+      },
+    },
+  ],
   developMiddleware: (app) => {
-    // VWorld API 프록시 설정
     app.use(
-        "/api", // 프록시 경로
-        createProxyMiddleware({
-          target: "https://api.vworld.kr", // VWorld API의 대상 URL
-          changeOrigin: true, // CORS 문제 해결
-          pathRewrite: {
-            "^/api": "", // "/api" 경로를 제거하고 실제 API 경로로 매핑
-          },
-        })
+      "/api",
+      createProxyMiddleware({
+        target: "https://api.vworld.kr",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": "",
+        },
+      })
     );
   },
 };
